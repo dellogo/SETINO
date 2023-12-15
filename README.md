499543a6cd372e24ca0466d8e1c61f16429d61ff
서영 <hongsy7179@gmail.com>
2023-12-15 16:38:54 +0900

    1.0.0

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
-# SETINO
-향수 브랜드 홈페이지
+![header](https://capsule-render.vercel.app/api?type=venom&color=auto&height=150&section=header&text=SETINO&fontSize=70)
+
+## **📖 목차**
+
+<b>
+  
+- 📝 [개요](#-포트폴리오-개요)
+- ⚒️ [기술 및 도구](#-기술-및-도구)
+- 📃 [작품 소개](#-작품-소개)
+</b>
+
+---
+
+## **📝 포트폴리오 개요**
+https://github.com/dellogo/SETINO/assets/93125060/b8cd15c5-d78e-4fb2-92b1-3c44e3209a05
+
+  > 프로젝트: SETINO
+  >
+  > 기획 및 제작: 홍서영
+  >
+  > 분류: 개인 프론트 프로젝트
+  >
+  > 주요 기능: 향수 브랜드 홈페이지
+  >
+  > 사용 기술: HTML, CSS, JavaScript, Visual Studio
+  >
+  > 문의: young_laon@naver.com 
+
+## **⚒️ 기술 및 도구**
+![HTML](https://img.shields.io/badge/HTML-239120?style=for-the-badge&logo=html5&logoColor=white) ![CSS](https://img.shields.io/badge/CSS-239120?&style=for-the-badge&logo=css3&logoColor=white) ![JavaScript](https://img.shields.io/badge/JavaScript-F7DF1E?style=for-the-badge&logo=JavaScript&logoColor=white) ![Visual Studio Code](https://img.shields.io/badge/Visual_Studio_Code-0078D4?style=for-the-badge&logo=visual%20studio%20code&logoColor=white) ![github](https://img.shields.io/badge/GitHub-100000?style=for-the-badge&logo=github&logoColor=white)
+
+## **📃 작품 소개**
+> ### 작품 소개
+> 
+> 욕실 가구 브랜드인 BATINO의 홈페이지를 벤치마킹해 제작하였습니다.<br>
+> 백엔드 작업 없이 HTML과 CSS, JavaScript를 사용해 제작한 프론트엔드 홈페이지입니다.<br>
+> 크게 메인페이지, 브랜드 소개 페이지, 상품 컬렉션 페이지, 컨택 페이지, 메뉴 페이지로 나뉘어져 있습니다.
---
+++ js/item_list.js
@@ -0,0 +1,64 @@
+const NEW_ITEM_LIST = [
+    {item_no: 1, src: 'scent0.jpg', item_title: 'WOODY_OIL_SCENT', item_price: '89000'}, 
+    {item_no: 2, src: 'scent1.jpg', item_title: 'CITRUS_SCENT', item_price: '25000'}, 
+    {item_no: 3, src: 'lotion.jpg', item_title: 'FLORAL_BODY', item_price: '30000'}, 
+    {item_no: 4, src: 'flower_n_scent.jpg', item_title: 'FLORAL_SCNET', item_price: '45000'}, 
+    {item_no: 5, src: 'musc_scent.jpg', item_title: 'MUSC_SCNET', item_price: '50000'}, 
+    {item_no: 6, src: 'oil.jpg', item_title: 'WOODY_OIL_FRAGRANCE', item_price: '68000'},
+    {item_no: 7, src: 'home_f.jpg', item_title: 'MUSC_BATH', item_price: '62000'},
+    {item_no: 8, src: 'lotion2.jpg', item_title: 'MUSC_BODY', item_price: '45000'},
+    {item_no: 9, src: 'home_f2.jpg', item_title: 'CITRUS_BATH', item_price: '20000'},
+    {item_no: 10, src: 'sea_scent3.jpg', item_title: 'WOODY_SCENT', item_price: '35000'}
+];
+
+const BEST_ITEM_LIST = [
+    {item_no: 1, src: 'scent0.jpg', item_title: 'WOODY_OIL_SCENT', item_price: '89000'}, 
+    {item_no: 2, src: 'scent1.jpg', item_title: 'CITRUS_SCENT', item_price: '25000'}, 
+    {item_no: 3, src: 'lotion.jpg', item_title: 'FLORAL_BODY', item_price: '30000'}, 
+    {item_no: 4, src: 'flower_n_scent.jpg', item_title: 'FLORAL_SCNET', item_price: '45000'}, 
+    {item_no: 5, src: 'musc_scent.jpg', item_title: 'MUSC_SCNET', item_price: '50000'}, 
+    {item_no: 6, src: 'oil.jpg', item_title: 'WOODY_OIL_FRAGRANCE', item_price: '68000'},
+    {item_no: 7, src: 'home_f.jpg', item_title: 'MUSC_BATH', item_price: '62000'},
+    {item_no: 8, src: 'lotion2.jpg', item_title: 'MUSC_BODY', item_price: '45000'},
+    {item_no: 9, src: 'home_f2.jpg', item_title: 'CITRUS_BATH', item_price: '20000'},
+    {item_no: 10, src: 'sea_scent3.jpg', item_title: 'WOODY_SCENT', item_price: '35000'}
+];
+
+const SCENT_ITEM_LIST = [
+    {item_no: 1, src: 'scent0.jpg', item_title: 'WOODY_OIL_SCENT', item_price: '89000'}, 
+    {item_no: 2, src: 'scent1.jpg', item_title: 'CITRUS_SCENT', item_price: '25000'}, 
+    {item_no: 3, src: 'flower_n_scent.jpg', item_title: 'FLORAL_SCNET', item_price: '45000'}, 
+    {item_no: 4, src: 'musc_scent.jpg', item_title: 'MUSC_SCNET', item_price: '50000'}, 
+    {item_no: 5, src: 'sea_scent3.jpg', item_title: 'WOODY_SCENT', item_price: '35000'},
+    {item_no: 6, src: 'scent0.jpg', item_title: 'WOODY_OIL_SCENT', item_price: '89000'}, 
+    {item_no: 7, src: 'scent1.jpg', item_title: 'CITRUS_SCENT', item_price: '25000'}, 
+    {item_no: 8, src: 'flower_n_scent.jpg', item_title: 'FLORAL_SCNET', item_price: '45000'}, 
+    {item_no: 9, src: 'musc_scent.jpg', item_title: 'MUSC_SCNET', item_price: '50000'}, 
+    {item_no: 10, src: 'sea_scent3.jpg', item_title: 'WOODY_SCENT', item_price: '35000'}
+];
+
+const BATH_BODY_ITEM_LIST = [
+    {item_no: 1, src: 'lotion.jpg', item_title: 'FLORAL_BODY', item_price: '30000'}, 
+    {item_no: 2, src: 'home_f.jpg', item_title: 'CITRUS_BATH', item_price: '62000'},
+    {item_no: 3, src: 'lotion2.jpg', item_title: 'MUSC_BODY', item_price: '45000'},
+    {item_no: 4, src: 'home_f2.jpg', item_title: 'MUSC_BATH', item_price: '20000'},
+    {item_no: 5, src: 'lotion3.jpg', item_title: 'WOODY_BODY', item_price: '35000'},
+    {item_no: 6, src: 'lotion.jpg', item_title: 'FLORAL_BODY', item_price: '30000'}, 
+    {item_no: 7, src: 'home_f.jpg', item_title: 'CITRUS_BATH', item_price: '62000'},
+    {item_no: 8, src: 'lotion2.jpg', item_title: 'MUSC_BODY', item_price: '45000'},
+    {item_no: 9, src: 'home_f2.jpg', item_title: 'MUSC_BATH', item_price: '20000'},
+    {item_no: 10, src: 'lotion3.jpg', item_title: 'WOODY_BODY', item_price: '35000'}
+];
+
+const HOME_F_ITEM_LIST = [
+    {item_no: 1, src: 'oil.jpg', item_title: 'WOODY_OIL_FRAGRANCE', item_price: '26000'},
+    {item_no: 2, src: 'fragrance1.jpg', item_title: 'CITRUS_FRAGRANCE', item_price: '24000'},
+    {item_no: 3, src: 'fragrance2.jpg', item_title: 'BASIL_FRAGRANCE', item_price: '32000'},
+    {item_no: 4, src: 'fragrance3.jpg', item_title: 'WOODY_FRAGRANCE', item_price: '30000'},
+    {item_no: 5, src: 'fragrance4.jpg', item_title: 'FLORAL_FRAGRANCE', item_price: '25000'},
+    {item_no: 6, src: 'oil.jpg', item_title: 'WOODY_OIL_FRAGRANCE', item_price: '26000'},
+    {item_no: 7, src: 'fragrance1.jpg', item_title: 'CITRUS_FRAGRANCE', item_price: '24000'},
+    {item_no: 8, src: 'fragrance2.jpg', item_title: 'BASIL_FRAGRANCE', item_price: '32000'},
+    {item_no: 9, src: 'fragrance3.jpg', item_title: 'WOODY_FRAGRANCE', item_price: '30000'},
+    {item_no: 10, src: 'fragrance4.jpg', item_title: 'FLORAL_FRAGRANCE', item_price: '25000'}
+]
